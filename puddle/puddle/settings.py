0bc82da463a8d3e135d5f5459ed7d58efc4fd4f2
rukshar69 <ruksharalam7@gmail.com>
2023-04-21 22:37:47 +0600

    coding

--- puddle/puddle/settings.py
+++ puddle/puddle/settings.py
@@ -27,6 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
 
 # Application definition
 
@@ -39,6 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'item',
+    'dashboard',
+    'conversation'
 ]
 
 MIDDLEWARE = [
--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -18,11 +18,13 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-from core.views import index, contact
+#from core.views import index, contact
 
 urlpatterns = [
 
-    path('', index, name='index'),
-    path('contact/', contact, name='contact'),
+    path('', include('core.urls')),
+    path('items/', include('item.urls')),
     path('admin/', admin.site.urls),
+    path('dashboard/', include('dashboard.urls')),
+    path('inbox/', include('conversation.urls'))
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
