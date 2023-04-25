74dfc1af7b5b860d5285b36ab1839344bc116ddb
rukshar69 <ruksharalam7@gmail.com>
2023-04-25 22:57:24 +0600

    modified wsgi and added vercel.json

--- puddle/puddle/wsgi.py
+++ puddle/puddle/wsgi.py
@@ -7,10 +7,17 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
+# import os
+
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'puddle.settings')
+
+# application = get_wsgi_application()
+
+
 import os
-
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'puddle.settings')
-
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "puddle.settings")
 application = get_wsgi_application()
+app = application
