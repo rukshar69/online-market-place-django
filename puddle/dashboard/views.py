5186fa36f1a07cdbdc584047d80e08779064b6d4
rukshar69 <ruksharalam7@gmail.com>
2023-06-23 22:11:25 +0600

    f

--- puddle/dashboard/views.py
+++ puddle/dashboard/views.py
@@ -6,7 +6,7 @@ from item.models import Item
 @login_required
 def index(request):
     items_not_approved = Item.objects.filter(created_by=request.user, is_allowed = False)
-    items_approved = Item.objects.filter(created_by=request.user, is_allowed = True)
+    items_approved = Item.objects.filter(created_by=request.user, is_allowed = True, is_sold=False)
     items_sold = Item.objects.filter(created_by=request.user, is_sold = True, is_allowed=True)
 
     return render(request, 'dashboard/index.html', {
